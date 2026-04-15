1894f4700abbee3e226bd3d82fd9732d278bf5e6
@itzzfalcon52 <hussaintaherkagalwala5253@gmail.com>
2026-04-15 21:22:24 +0530

    added env variable for backend url

--- apps/frontend/src/api/axios.ts
+++ apps/frontend/src/api/axios.ts
@@ -2,6 +2,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://spons-crm-api.sponscrm.tech/api/v1", // change in prod
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api/v1', // change in prod
   withCredentials: true, //  REQUIRED for cookies
 });
