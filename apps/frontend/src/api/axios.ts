af05b2b9aa5a5333b54ec602f4c0434ffb7e6400
@itzzfalcon52 <hussaintaherkagalwala5253@gmail.com>
2026-04-15 20:48:56 +0530

    updated the backend domain url

--- apps/frontend/src/api/axios.ts
+++ apps/frontend/src/api/axios.ts
@@ -2,6 +2,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1", // change in prod
+  baseURL: "https://spons-crm-api.sponscrm.tech/api/v1", // change in prod
   withCredentials: true, //  REQUIRED for cookies
 });
