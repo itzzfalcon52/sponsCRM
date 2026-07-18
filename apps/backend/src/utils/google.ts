c127a6dc3f041a2df7374275b687963168b49025
Hussain Taher Kagalwala <hussaintaherkagalwala5253@gmail.com>
2026-07-18 16:55:49 +0530

    Use environment variable for Google redirect URI

--- apps/backend/src/utils/google.ts
+++ apps/backend/src/utils/google.ts
@@ -6,6 +6,6 @@ export const getOAuthClient = () => {
   return new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    "http://localhost:3000/api/v1/google/callback" 
+    process.env.GOOGLE_REDIRECT_URI
   );
 };
